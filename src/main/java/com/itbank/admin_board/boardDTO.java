2c599edf43bd38cce3801caebf0e6828ef423bcf
junsup1959 <junsup1959@naver.com>
2021-06-16 01:19:00 +0900

    DTO 수정

--- src/main/java/com/itbank/admin_board/boardDTO.java
+++ src/main/java/com/itbank/admin_board/boardDTO.java
@@ -10,26 +10,6 @@ public class boardDTO {
 	
 	private final String root="D:\\upload\\";
 	private int board_number,board_prev,board_next;
-	public int getBoard_prev() {
-		return board_prev;
-	}
-
-
-	public void setBoard_prev(int board_prev) {
-		this.board_prev = board_prev;
-	}
-
-
-	public int getBoard_next() {
-		return board_next;
-	}
-
-
-	public void setBoard_next(int board_next) {
-		this.board_next = board_next;
-	}
-
-
 	private String board_content,board_bdate,board_edate,board_title,board_store,board_file,admin_num;
 	private char board_notice;
 	MultipartFile file;
@@ -53,7 +33,25 @@ public class boardDTO {
 			this.board_file = fileName;
 		}
 	}
-	
+
+	public int getBoard_prev() {
+		return board_prev;
+	}
+
+
+	public void setBoard_prev(int board_prev) {
+		this.board_prev = board_prev;
+	}
+
+
+	public int getBoard_next() {
+		return board_next;
+	}
+
+
+	public void setBoard_next(int board_next) {
+		this.board_next = board_next;
+	}
 	
 	public int getBoard_number() {
 		return board_number;
