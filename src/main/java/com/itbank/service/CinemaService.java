696f8acbb9e57a1f6af77f19a682cf08bd7184e8
junsup1959 <82262248+junsup1959@users.noreply.github.com>
2021-06-17 16:25:27 +0900

    Add files via upload
    파일 빠져있음

---
+++ src/main/java/com/itbank/service/CinemaService.java
@@ -0,0 +1,39 @@
+package com.itbank.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itbank.theater.TheaterDAO;
+import com.itbank.theater.TheaterDTO;
+
+@Service
+public class CinemaService {
+
+	@Autowired private TheaterDAO dao;
+
+	public List<TheaterDTO> selectAll() {
+		// TODO Auto-generated method stub
+		return dao.selectAll();
+	}
+
+	public int insert(TheaterDTO dto) {
+		String branchcode=dto.getBranch();
+		branchcode += UUID.randomUUID().toString().split("-")[0];
+		dto.setBranchcode(branchcode);
+		return dao.insert(dto);
+		}
+
+	public TheaterDTO selectOne(String branchcode) {
+		// TODO Auto-generated method stub
+		return dao.selectOne(branchcode);
+	}
+
+	public int update(TheaterDTO dto) {
+		// TODO Auto-generated method stub
+
+		return dao.update(dto);
+	}
+}
